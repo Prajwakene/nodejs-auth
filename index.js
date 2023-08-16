e9720510142297f774663e67ea34bc4dc8c1f3c4
Prajwal Kene <prajwalnk123@gmail.com>
2023-08-16 20:05:00 +0530

    authetication-shortID

---
+++ index.js
@@ -0,0 +1,53 @@
+// for .env
+require("dotenv").config();
+
+
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require("./connect");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const URL = require("./models/url");
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+connectToMongoDB(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/nodejs-authentication")
+.then(() =>
+  console.log("Mongodb connected")
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
+
+app.get("/url/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server is runing on port :${PORT}`));
---
+++ models/url.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirectURL: {
+      type: String,
+      required: true,
+    },
+    visitHistory: [{ timestamp: { type: Number } }],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+  },
+  { timestamps: true }
+);
+
+const URL = mongoose.model("mongodb://localhost:27017/nodejs-authentication", urlSchema);
+// mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = URL;
